6086a3a5b5d57b3496f2810b4cacc2875a3b6ea7
uonr <dev@yuru.me>
2024-04-14 17:39:24 +0900

    chore(build): split deps

--- flake.nix
+++ flake.nix
@@ -14,23 +14,52 @@
     let
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages."${system}";
+      nodeDeps =
+        napalm.legacyPackages."${system}".buildPackage
+          # Keeps only package.json and package-lock.json files
+          (pkgs.lib.sourceByRegex ./. [
+            ".*package.json"
+            ".*package-lock.json"
+            "(apps|packages)"
+            "(apps|packages)/[a-zA-Z0-9_-]+"
+          ])
+          {
+            buildInputs = with pkgs; [ cacert ];
+            npmCommands = [ "npm ci --loglevel verbose --nodedir=${pkgs.nodejs}/include/node" ];
+          };
     in
     {
-      # Assuming the flake is in the same directory as package-lock.json
-      packages."${system}".boluo = napalm.legacyPackages."${system}".buildPackage ./. {
-        buildInputs = with pkgs; [
-          cacert
+
+      packages."${system}".boluo = pkgs.stdenv.mkDerivation {
+        name = "boluo";
+        src = ./.;
+        version = "0.0.0";
+        buildInputs = [
+          nodeDeps
+          pkgs.nodejs
+          pkgs.cacert
         ];
-        certEnv = [
-          "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-        ];
-        npmCommands = [
-          "npm install --loglevel verbose --nodedir=${pkgs.nodejs}/include/node"
-          "npm exec turbo telemetry disable"
-          "npm exec turbo build -- --no-cache --verbosity=3 --no-daemon --no-update-notifier"
-        ];
+
+        configurePhase = ''
+          runHook preConfigure
+
+          export HOME=$(mktemp -d)
+
+          runHook postConfigure
+        '';
+        buildPhase = ''
+          runHook preBuild
+          cp -r ${nodeDeps}/_napalm-install/node_modules .
+          chmod -R u+w .
+          npm run build
+          runHook postBuild
+        '';
+        installPhase = ''
+          mkdir -p $out
+          cp -r apps/web/.next/* $out
+        '';
       };
 
-      devShells."${system}".shell-name = pkgs.mkShell { nativeBuildInputs = with pkgs; [ nodejs ]; };
+      devShells."${system}".default = pkgs.mkShell { nativeBuildInputs = with pkgs; [ nodejs ]; };
     };
 }
